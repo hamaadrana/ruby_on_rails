7033f312c586297d15dafd3547c8addf64c649b2
hamadrana23 <hamadrana23@gmail.com>
2018-10-02 19:29:43 +0500

    Learning MVC without scaffold Added Articles Model and Controller for create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002120908) do
+ActiveRecord::Schema.define(version: 20181002131227) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
